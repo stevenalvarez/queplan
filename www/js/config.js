48b09a2772bac4aae3fde10767105d609975b19c
jhonny <steven.alvarez.v@gmail.com>
2013-11-04 11:56:17 -0400

    base url app

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 /************************************ VARIABLES DE CONFIGURACION *******************************************************/
 
 /************************************ server *******************************************************/
-//var BASE_URL_APP = "http://www.terrazas.arrobacreativa.com/";
+var BASE_URL_APP = "http://www.terrazas.arrobacreativa.com/";
 
 /************************************ localhost *******************************************************/
-var BASE_URL_APP = "http://localhost/queplan/web/";
+//var BASE_URL_APP = "http://localhost/queplan/web/";
