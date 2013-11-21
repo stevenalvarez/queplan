ac0c657c7ca2eed6d85d25607186af10312a4c06
jhonny <steven.alvarez.v@gmail.com>
2013-11-21 14:20:33 -0400

    comenzamos con pushNotificacion

--- www/js/app.ready.js
+++ www/js/app.ready.js
@@ -48,5 +48,63 @@ var app = {
         
         //Inicializamos el api de twitter
         cb.setConsumerKey(consumer_key, consumer_secret);
+        
+        //Inicializamos el pushNotification
+        var pushNotification = window.plugins.pushNotification;
+        if (device.platform == 'android' || device.platform == 'Android') {
+            alert("Register called android");
+            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"629734064389","ecb":"app.onNotificationGCM"});
+        }
+        else {
+            alert("Register called ios");
+            pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
+        }        
+    },
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        console.log("Regid " + result);
+        alert('Callback Success! Result = '+result);
+    },
+    errorHandler:function(error) {
+        alert(error);
+    },
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    },
+    onNotificationAPN: function(event) {
+        var pushNotification = window.plugins.pushNotification;
+        
+        if (event.alert) {
+            navigator.notification.alert(event.alert);
+        }
+        if (event.badge) {
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge);
+        }
+        if (event.sound) {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
     }
 };
