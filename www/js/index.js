da2b1042151293f89031879caf2b549e6e012eb7
jhonny <steven.alvarez.v@gmail.com>
2014-08-03 14:10:27 -0400

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,26 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ 
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
+
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}  
+ 
 var app = {
     // Application Constructor
     initialize: function() {
@@ -46,12 +66,6 @@ var app = {
 
         console.log('Received Event: ' + id);
         
-        navigator.geolocation.getCurrentPosition(
-      function(position) {
-          alert(position.coords.latitude + ',' + position.coords.longitude);
-      },
-      function() {
-          alert('Error getting location');
-      });        
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true});
     }
 };
